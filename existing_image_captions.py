32ec0d03a4b0297a8a740ce189aa3290eafc2d35
Wesam Alwehaibi <46692359+walwehaibi@users.noreply.github.com>
2019-11-09 19:37:54 -0800

    Update existing_image_captions.py

--- existing_image_captions.py
+++ existing_image_captions.py
@@ -13,6 +13,29 @@ images_dir = '/Users/wesam/PycharmProjects/DeepLearningProject/venv/images'
 sis = vist.Story_in_Sequence(images_dir,vist_dir)
 
 
+
+#generate a file that contains all captions
+json_dict={}
+for album_id in sis.Albums:
+    if(album_id not in json_dict):
+        json_dict[album_id]={}
+    for story_id in sis.Albums[album_id]['story_ids']:
+        #sis.show_story(story)
+        if (story_id not in json_dict[album_id]):
+            json_dict[album_id][story_id]={}
+        story = sis.Stories[story_id]
+        list_cap=[]
+        sent_ids = story['sent_ids']
+        for sent_id in sent_ids:
+            sent = sis.Sents[sent_id]
+            list_cap.append((sent['img_id'],sent['text']))
+        json_dict[album_id][story_id]=list_cap
+with open('all_captions.txt', 'w') as outfile:
+    json.dump(json_dict,outfile)
+
+
+
+
 #check if images exist in the downloaded splits
 existing_imgs_albums=[]
 for img in sis.images:
@@ -20,7 +43,7 @@ for img in sis.images:
     if os.path.isfile(path):
         if img['album_id'] not in existing_imgs_albums:
             existing_imgs_albums.append(img['album_id'])
-
+#access one story
 album_id = existing_imgs_albums[1]
 story_ids = sis.Albums[album_id]['story_ids']
 story_id = story_ids[1]
